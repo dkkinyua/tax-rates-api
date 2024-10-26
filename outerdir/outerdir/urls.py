90ef91bcf230324526495cf4ae46f0c0458892b7
unknown <denzelkinyua11@gmail.com>
2024-10-26 16:27:19 +0300

    Commit: Added URL path to home and api/

--- outerdir/outerdir/urls.py
+++ outerdir/outerdir/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from core import views
 
 urlpatterns = [
+    path('', views.index, name='index_page'),
     path('api/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
