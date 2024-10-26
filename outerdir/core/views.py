e4a52c4b117ce0923d3299e77a901bb99d531b25
unknown <denzelkinyua11@gmail.com>
2024-10-26 16:28:53 +0300

    Commit: Added tax rates for countries, will add sth t be updating this on a daily

--- outerdir/core/views.py
+++ outerdir/core/views.py
@@ -1,3 +1,22 @@
+import os
+import json
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+
+from rest_framework.decorators import api_view
+from django_ratelimit.decorators import ratelimit
 
 # Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+@ratelimit(key='ip', rate='20/m', block=True)
+@api_view(['GET'])
+def get_tax_rates(request):
+
+    with open("core/data/tax.json", 'r') as f:
+        data = json.load(f)
+
+    return JsonResponse(data)
