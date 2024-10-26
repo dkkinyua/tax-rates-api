21bfd2a9308bf5bf9481865a9c0b5d24afe65b29
unknown <denzelkinyua11@gmail.com>
2024-10-26 17:07:38 +0300

    Commit: Made changes to tax.json

--- outerdir/core/views.py
+++ outerdir/core/views.py
@@ -22,7 +22,7 @@ def get_tax_rates(request):
     state = request.GET.get('state')
 
     if state:
-        state_data = data.get(state)
+        state_data = data[state]['combined_rate']
         if state_data:
             return JsonResponse({state: state_data})
         else:
